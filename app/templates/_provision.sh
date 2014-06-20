588e26ae9b114249039bab7712b971e19aabc89c
egasimus <fallenblood@gmail.com>
2014-06-20 18:33:49 +0300

    Remove all references to old project name

--- app/templates/_provision.sh
+++ app/templates/_provision.sh
@@ -10,12 +10,12 @@ SECRET_KEY=$SECRET_KEY" | tee -a /etc/environment
 
 # Create system user and group
 groupadd <%= projectGroup %>
-useradd <%= projectUser %> -g <%= projectGroup %> || usermod s4c -g <%= projectGroup %>
+useradd <%= projectUser %> -g <%= projectGroup %> || usermod <%= projectUser %> -g <%= projectGroup %>
 useradd vagrant -G <%= projectGroup %> || usermod vagrant -G <%= projectGroup %>
 useradd ubuntu -G <%= projectGroup %> || usermod ubuntu -G <%= projectGroup %>
 
 
-# Alias /vagrant as /www/s4c-cms
+# Alias /vagrant as <%= wwwDir %>
 mkdir /www && ln -s /vagrant <%= wwwDir %>
 
 
@@ -25,30 +25,30 @@ pip install --requirement <%= wwwDir %>/app-requirements.txt
 
 
 # Prepare app's static, media, and log directories.
-mkdir -p /www/media /www/static /var/log/s4c
-chmod 774 /www/media /www/static /var/log/s4c
+mkdir -p /www/media /www/static <%= logDir %>
+chmod 774 /www/media /www/static <%= logDir %>
 chown <%= projectUser %>:<%= projectGroup %> /www/media
 
 
 # Prepare log files.
-touch /var/log/s4c/app.log /var/log/s4c/db.log \
-      /var/log/s4c/request.log /var/log/s4c/test.log
-chown --recursive s4c:s4c /var/log/s4c
-chmod 664 /var/log/s4c/*
+touch <%= logDir %>/app.log <%= logDir %>/db.log \
+      <%= logDir %>/request.log <%= logDir %>/test.log
+chown --recursive <%= projectUser %>:<%= projectGroup %> <%= logDir %>
+chmod 664 <%= logDir %>/*
 
 
 # Collect static files and set permissions
-python /www/s4c-cms/manage.py collectstatic -c --noinput
-chown --recursive s4c:s4c /www/static
+python <%= wwwDir %>/manage.py collectstatic -c --noinput
+chown --recursive <%= projectUser %>:<%= projectGroup %> /www/static
 find /www/static -type f -exec chmod 664 {} +
 
 
 # Create a MySQL database and user
 mysql -u root --password=thisisthedefaultmysqlrootpassword -e \
-    "CREATE DATABASE s4c DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;"
+    "CREATE DATABASE <%= dbName %> DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;"
 
 mysql -u root --password=thisisthedefaultmysqlrootpassword -e \
-    "GRANT ALL PRIVILEGES ON s4c.* To 's4c'@'localhost' IDENTIFIED BY '$DB_PASSWORD';"
+    "GRANT ALL PRIVILEGES ON <%= dbName %>.* To '<%= dbUser %>'@'localhost' IDENTIFIED BY '$DB_PASSWORD';"
 
 
 # Install PhantomJS
@@ -57,27 +57,27 @@ mysql -u root --password=thisisthedefaultmysqlrootpassword -e \
 
 
 # Install and configure nginx and uwsgi
-usermod www-data -G s4c || useradd www-data -G s4c
-usermod nginx -G s4c || useradd nginx -G s4c
-useradd uwsgi -G s4c || usermod uwsgi -G s4c
+usermod www-data -G <%= projectGroup %> || useradd www-data -G <%= projectGroup %>
+usermod nginx -G <%= projectGroup %> || useradd nginx -G <%= projectGroup %>
+useradd uwsgi -G <%= projectGroup %> || usermod uwsgi -G <%= projectGroup %>
 mkdir -p /var/log/uwsgi
-chmod 774 /var/log/s4c /var/log/nginx /var/log/uwsgi
-chown --recursive nginx:s4c /var/log/nginx
-chown --recursive uwsgi:s4c /var/log/uwsgi
+chmod 774 <%= logDir %> /var/log/nginx /var/log/uwsgi
+chown --recursive nginx:<%= projectGroup %> /var/log/nginx
+chown --recursive uwsgi:<%= projectGroup %> /var/log/uwsgi
 
 
 # Create database and Postgres user
-# sudo -u postgres createdb s4c -E=utf8
-# sudo -u postgres createuser s4c -d
+# sudo -u postgres createdb <%= dbName %> -E=utf8
+# sudo -u postgres createuser <%= dbUser %> -d
 
 
 # Sync DB and load initial data
-python /www/s4c-cms/manage.py syncdb --noinput
-python /www/s4c-cms/manage.py migrate
+python <%= wwwDir %>/manage.py syncdb --noinput
+python <%= wwwDir %>/manage.py migrate
 
 
 # Upload crontab
-cp /www/s4c-cms/deploy/crontab.conf /etc/cron.d/s4c
+cp <%= wwwDir %>/deploy/crontab.conf /etc/cron.d/<%= projectName %>
 
 
 # Configure and launch uWSGI
@@ -85,13 +85,13 @@ cp /vagrant/deploy/uwsgi_upstart.conf /etc/init/uwsgi.conf
 chmod 0644 /etc/init/uwsgi.conf
 mkdir -p /etc/uwsgi/vassals
 chmod 770 /etc/uwsgi/vassals
-chown --recursive uwsgi:s4c /etc/uwsgi/vassals
-ln -s /vagrant/deploy/uwsgi_dev.ini /etc/uwsgi/vassals/s4c-cms.ini
+chown --recursive uwsgi:<%= projectGroup %> /etc/uwsgi/vassals
+ln -s /vagrant/deploy/uwsgi_dev.ini /etc/uwsgi/vassals/<%= projectName %>.ini
 service uwsgi restart
 
 
 # Configure and launch nginx
-ln -s /vagrant/deploy/nginx_dev.conf /etc/nginx/sites-available/s4c-cms
-ln -s /etc/nginx/sites-available/s4c-cms /etc/nginx/sites-enabled/
+ln -s /vagrant/deploy/nginx_dev.conf /etc/nginx/sites-available/<%= projectName %>
+ln -s /etc/nginx/sites-available/<%= projectName %> /etc/nginx/sites-enabled/
 unlink /etc/nginx/sites-enabled/default
 service nginx restart
